b709e38a710da0abd34b384b9c410168c8182683
Thomas <thomas-hiemstra@hotmail.com>
2024-12-15 13:08:31 +0100

    servo stuff

---
+++ STServo_Python/write.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# *********     Gen Write Example      *********
+#
+#
+# Available STServo model on this example : All models using Protocol STS
+# This example is tested with a STServo and an URT
+#
+
+import sys
+import os
+
+sys.path.append("..")
+from STservo_sdk import *                 # Uses STServo SDK library
+import time
+
+# Default setting
+STS_ID                      = 1                 # STServo ID : 1
+BAUDRATE                    = 1000000           # STServo default baudrate : 1000000
+DEVICENAME                  = 'COM5'    # Check which port is being used on your controller
+                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+STS_MINIMUM_POSITION_VALUE  = 0           # STServo will rotate between this value
+STS_MAXIMUM_POSITION_VALUE  = 4095
+STS_MOVING_SPEED            = 44000        # STServo moving speed
+STS_MOVING_ACC              = 250         # STServo moving acc
+
+index = 0
+sts_goal_position = [STS_MINIMUM_POSITION_VALUE, STS_MAXIMUM_POSITION_VALUE]         # Goal position
+
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+portHandler = PortHandler(DEVICENAME)
+
+# Initialize PacketHandler instance
+# Get methods and members of Protocol
+packetHandler = sts(portHandler)
+    
+# Open port
+if portHandler.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+    print("Press any key to terminate...")
+    getch()
+    quit()
+
+# Set port baudrate
+if portHandler.setBaudRate(BAUDRATE):
+    print("Succeeded to change the baudrate")
+else:
+    print("Failed to change the baudrate")
+    print("Press any key to terminate...")
+    quit()
+
+while 1:
+    input("Press Enter to continue...")
+
+    # Write STServo goal position/moving speed/moving acc
+    print("writing pos")
+    sts_comm_result, sts_error = packetHandler.WritePosEx(STS_ID, sts_goal_position[index], STS_MOVING_SPEED, STS_MOVING_ACC)
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(sts_comm_result))
+    if sts_error != 0:
+        print("%s" % packetHandler.getRxPacketError(sts_error))
+
+    # Change goal position
+    if index == 0:
+        index = 1
+    else:
+        index = 0
+
+# Close port
+portHandler.closePort()
