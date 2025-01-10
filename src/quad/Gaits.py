cb21746521248980686ef62628d3cda0184209da
Thomas <thomas-hiemstra@hotmail.com>
2025-01-10 13:03:12 +0100

    cleanup

--- src/quad/Gaits.py
+++ src/quad/Gaits.py
@@ -2,7 +2,6 @@ class GaitController:
     def __init__(self, config):
         self.config = config
 
-
     def phase_index(self, ticks):
         """Calculates which part of the gait cycle the robot should be in given the time in ticks.
         
@@ -26,7 +25,6 @@ class GaitController:
                 return i
         assert False
 
-
     def subphase_ticks(self, ticks):
         """Calculates the number of ticks (timesteps) since the start of the current phase.
 
@@ -44,14 +42,12 @@ class GaitController:
         """
         phase_time = ticks % self.config.phase_length
         phase_sum = 0
-        subphase_ticks = 0
         for i in range(self.config.num_phases):
             phase_sum += self.config.phase_ticks[i]
             if phase_time < phase_sum:
                 subphase_ticks = phase_time - phase_sum + self.config.phase_ticks[i]
                 return subphase_ticks
         assert False
-
 
     def contacts(self, ticks):
         """Calculates which feet should be in contact at the given number of ticks
